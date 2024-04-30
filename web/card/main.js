0018c0026844b08ec0f2d7647b42db12cf388fc6
Boris Smus <boris@smus.com>
2024-04-30 11:39:16 -0700

    mtg-style wip

---
+++ web/card/main.js
@@ -0,0 +1,36 @@
+import { renderCard, renderMTGCard, cardWidth, cardHeight } from "../card.js";
+import { loadGraph } from "../utils.js";
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+async function main() {
+  const svg = d3
+    .select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("viewBox", [0, 0, width, height]);
+  const nodeSvg = svg.append("g").attr("class", "nodes");
+
+  const { nodes, edges } = await loadGraph("../asimov-1700.csv");
+  console.log(`Loaded ${nodes.length} inventions.`);
+
+  const myNodes = [nodes[145]]
+  // const myNodes = nodes
+
+  const cards = nodeSvg.selectAll("g").data(myNodes, (d) => d.index);
+
+  const cardEnter = cards.enter();
+  const card = renderMTGCard(cardEnter);
+  card.attr("transform", `translate(${cardWidth / 2 + 2}, ${cardHeight / 2 + 2})`);
+}
+
+function onResize() {
+  const svg = d3
+    .select("#container svg")
+    .attr("width", width)
+    .attr("viewBox", [0, 0, width, height]);
+}
+
+window.addEventListener("DOMContentLoaded", main);
+window.addEventListener("resize", onResize);

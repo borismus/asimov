ccff229f896ffbf6a4fd28d9228fc48e0229a6d5
Boris Smus <boris@smus.com>
2025-01-15 17:43:51 -0800

    refix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 DEPLOY_DIR=~/Projects/invention.cards/
-python3 scripts/generate-site.py -o $DEPLOY_DIR -n
+python3 scripts/generate-site.py -o $DEPLOY_DIR
 cd $DEPLOY_DIR
 git add -A
 git commit . -m 'deploying to gh-pages via script'
